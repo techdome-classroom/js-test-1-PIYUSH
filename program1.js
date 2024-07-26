3ce3378d876431921b62dfe748c705b7d8e87c95
Piyush Patel <139048654+itsasolution@users.noreply.github.com>
2024-07-26 13:42:34 +0530

    Update program1.js

--- program1.js
+++ program1.js
@@ -2,6 +2,33 @@ const getTotalIsles = function (grid) {
 
 
   // write your code here
+  if (!grid || grid.length === 0) return 0;
+
+  let count = 0; // for island
+  const rows = grid.length;
+  const cols = grid[0].length;
+
+  function findIsland(r, c) {
+    if (r < 0 || c < 0 || r >= rows || c >= cols || grid[r][c] === 'W') return;
+
+    grid[r][c] = 'W';  // land visited memoize it
+    findIsland(r + 1, c);
+    findIsland(r - 1, c);
+    findIsland(r, c + 1);
+    findIsland(r, c - 1);
+  }
+
+  // looping for each island
+  for (let r = 0; r < rows; r++) {
+    for (let c = 0; c < cols; c++) {
+      if (grid[r][c] === 'L') {
+        count++;
+        findIsland(r, c);
+      }
+    }
+  }
+
+  return count;
 
 };
 
